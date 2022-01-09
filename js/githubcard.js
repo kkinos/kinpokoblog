6b6c7d6af4cfb5db0bc6142315e82facfc3a4d10
kinpoko <kinpoko@users.noreply.github.com>
2022-01-09 13:35:49 +0000

    deploy: 03d0c6603891b83c4580b1a3bc3355fb058e1cd6

--- js/githubcard.js
+++ js/githubcard.js
@@ -1,7 +1,7 @@
 (() => {
   // <stdin>
-  MakeGitHubCard(Url, Name);
-  function MakeGitHubCard(url, name) {
+  MakeGithubCard(Url, Name);
+  function MakeGithubCard(url, name) {
     var repo_name = name;
     fetch("https://getogp.vercel.app/api?url=" + url, {
       mode: "cors"
@@ -14,18 +14,18 @@
     });
   }
   function MakeCard(jsonObj, name) {
-    const nameele = document.getElementById(name);
+    const target_ele = document.getElementById(name);
     const title = jsonObj.title;
     const url = jsonObj.url;
     const description = jsonObj.description;
-    const imageurl = jsonObj.image;
-    var repourl = document.createElement("a");
-    repourl.href = url;
-    var img = document.createElement("img");
-    img.src = imageurl;
-    img.style.width = "70%";
-    img.style.height = "70%";
-    repourl.appendChild(img);
-    nameele.appendChild(repourl);
+    const image_url = jsonObj.image;
+    var ele_a = document.createElement("a");
+    ele_a.href = url;
+    var ele_img = document.createElement("img");
+    ele_img.src = image_url;
+    ele_img.style.width = "70%";
+    ele_img.style.height = "70%";
+    ele_a.appendChild(ele_img);
+    target_ele.appendChild(ele_a);
   }
 })();
