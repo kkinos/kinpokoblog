08c8dd46e13069d05b10a32565d5ba21f1c2a2c9
kinpoko <kinpoko@users.noreply.github.com>
2022-01-09 01:04:37 +0000

    deploy: 26770ed5efea739e5c7713fb25413ed14a142b10

---
+++ js/githubcard.js
@@ -0,0 +1,32 @@
+(() => {
+  // <stdin>
+  MakeGitHubCard(Url, Name);
+  function MakeGitHubCard(url, name) {
+    var URL = url;
+    var repo_name = name;
+    fetch("https://getogp.vercel.app/api?url=" + Url, {
+      mode: "cors"
+    }).then((response) => {
+      return response.json();
+    }).then((result) => {
+      MakeCard(result, repo_name);
+    }).catch((e) => {
+      console.log(e);
+    });
+  }
+  function MakeCard(jsonObj, name) {
+    const nameele = document.getElementById(name);
+    const title = jsonObj.title;
+    const url = jsonObj.url;
+    const description = jsonObj.description;
+    const imageurl = jsonObj.image;
+    var repourl = document.createElement("a");
+    repourl.href = url;
+    var img = document.createElement("img");
+    img.src = imageurl;
+    img.style.width = "70%";
+    img.style.height = "70%";
+    repourl.appendChild(img);
+    nameele.appendChild(repourl);
+  }
+})();
